587fae91a9b383332c15242a69e0b4896540add8
unniisme <122001019@smail.iitpkd.ac.in>
2023-07-18 23:44:11 +0530

    Merge with main on wizard

---
+++ wizard_options.sh
@@ -0,0 +1,13 @@
+
+BASHRC="$HOME/.bashrc"
+VIMRC="$HOME/.vimrc"
+VIMPACK="$HOME/.vim/pack"
+GITCONFIG="$HOME/.gitconfig"
+
+# vim packages
+PACKAGES=(
+  'altercation/vim-colors-solarized' 
+  'vim-airline/vim-airline' 
+  'preservim/nerdtree'
+  'airblade/vim-gitgutter'
+)
